1be334ed05c2d5811a3f42421664ee616268d2d7
gmind7 <gmind7@gmail.com>
2014-01-02 11:05:50 +0900

    ehcache 테스트 코드 추가

---
+++ spring-ehcache-config/src/test/main/com/gmind7/bakery/test/ehcache/Baker.java
@@ -0,0 +1,70 @@
+package com.gmind7.bakery.test.ehcache;
+
+public class Baker {
+	
+	private long id;
+	
+    private String username;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int)(id ^ (id >>> 32));
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Baker other = (Baker)obj;
+		if (id != other.id)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Baker [id=" + id + ", username=" + username + "]";
+	}
+	
+}
